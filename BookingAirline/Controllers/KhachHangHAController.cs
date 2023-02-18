eb5b3c9d867fb085cd0445fbbfaa13abf31a09ea
Trương Tấn Nhã <truongtannha2702@gmail.com>
2023-02-18 22:42:49 +0700

    Update 18/02/2023

--- BookingAirline/Controllers/KhachHangHAController.cs
+++ BookingAirline/Controllers/KhachHangHAController.cs
@@ -25,9 +25,34 @@ namespace BookingAirline.Controllers
             var data = database.KhachHangs.Where(s => s.IDKH == user.IDKH).FirstOrDefault();
             return View(data);
         }
+        [HttpGet]
+        public ActionResult EditProfile(string id)
+        {
+            KhachHang kh = database.KhachHangs.Find(id);
+            ViewBag.MaLKH = new SelectList(database.LoaiKHs, "MaLKH", "TenLKH");
+            return View(kh);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditProfile([Bind(Include = "IDKH,MaLKH,UserName,Password,TenKH,SDT,Email,GioiTinh,NgaySinh")] KhachHang kh )
+        {
+            //database.Entry(kh).State = System.Data.Entity.EntityState.Modified;
+            //database.SaveChanges();
+            //return RedirectToAction("ThongtinCaNhan");
+            if (ModelState.IsValid)
+            {
+                database.Entry(database.KhachHangs.Find(kh.IDKH)).CurrentValues.SetValues(kh);
+                database.SaveChanges();
+                return View("ThongTinKH");
+            }
+            return View(kh);
+        }
         public ActionResult Booking()
         {
-            return View();
+            var user = (BookingAirline.Models.KhachHang)Session["userKH"];
+            var data = database.Ves.Where(s => s.IDKH == user.IDKH).ToList();
+            return View(data);
         }
         public ActionResult Whishlist()
         {
