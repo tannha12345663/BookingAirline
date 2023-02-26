fd98b680087a3ce6380291baf8e0a79b2c0f0cbf
Trương Tấn Nhã <33920360+tannha12345663@users.noreply.github.com>
2023-02-26 18:29:06 +0700

    Update 26/02/2023

--- BookingAirline/Controllers/NhanVienController.cs
+++ BookingAirline/Controllers/NhanVienController.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using BookingAirline.App_Start;
 namespace BookingAirline.Controllers
 {
+    [AuthenticationNV]
     public class NhanVienController : Controller
     {
-
         // GET: NhanVien
         public ActionResult TrangChu()
+        {
+            return View();
+        }
+        public ActionResult Scheduleaflight()
         {
             return View();
         }
