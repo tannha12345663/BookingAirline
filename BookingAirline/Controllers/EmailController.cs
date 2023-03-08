c1c96a32528d5fcc082ad1aefba3f94172848a2f
Trương Tấn Nhã <33920360+tannha12345663@users.noreply.github.com>
2023-03-08 11:07:43 +0700

    update 08/03/2023

--- BookingAirline/Controllers/EmailController.cs
+++ BookingAirline/Controllers/EmailController.cs
@@ -18,7 +18,7 @@ namespace BookingAirline.Controllers
         public ActionResult SendEmail(string useremail)
         {
             string subject = "Đây là tin nhắn tự động từ hệ thống POS";
-            string body = "Xin chào Diễm Khang bạn vừa đặng nhập , Nếu không phải là bạn vui lòng kiểm tra lại tài khoản tại .... !";
+            string body = "Xin chào bạn <h1>Nguyễn Thị Diễm Quỳnh </h1>bạn vừa đặng nhập vào trang web tội phạm , Nếu không phải là bạn vui lòng cung cấp thông tin của bạn tại https://luatduonggia.vn/cach-lam-don-gui-don-to-cao-online-qua-mang-moi-nhat/. !";
             WebMail.Send(useremail, subject, body,null,null,null,true,null,null,null,null,null,null);
             ViewBag.msg = "Email đã được gửi thành công ...";
             return View();
